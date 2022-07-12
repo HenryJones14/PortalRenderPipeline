9160bf8e17e532944dca90c12bbe206f2158884e
Domantas Martinaitis <domantas.martinaitis@gmail.com>
2022-07-12 23:05:19 +0200

    bob

--- Runtime/ForwardRenderer.cs
+++ Runtime/ForwardRenderer.cs
@@ -460,7 +460,7 @@ namespace UnityEngine.Rendering.Universal
             if (camera.clearFlags == CameraClearFlags.Skybox && (RenderSettings.skybox != null || cameraSkybox?.material != null) && !isOverlayCamera)
                 EnqueuePass(m_DrawSkyboxPass);
 
-            if (requiresDepthCopyPass) // Portal change
+            if (false || requiresDepthCopyPass) // Portal change
             {
                 m_CopyDepthPass.Setup(m_ActiveCameraDepthAttachment, m_DepthTexture);
                 EnqueuePass(m_CopyDepthPass);
